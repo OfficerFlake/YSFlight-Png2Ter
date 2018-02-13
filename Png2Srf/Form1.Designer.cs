8afbdb227db61893a3177645cd5ed7feafc1eb54
Dwayne Brumby <Dwayne.Brumby@outlook.com>
2018-02-13 18:29:20 +1100

    Add project files.

---
+++ Png2Srf/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Png2Srf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DropLabel = new System.Windows.Forms.Label();
+            this.textBox_MaxNodeHeight = new System.Windows.Forms.TextBox();
+            this.textBox_Width = new System.Windows.Forms.TextBox();
+            this.textBox_Height = new System.Windows.Forms.TextBox();
+            this.label_MaxNodeHeight = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // DropLabel
+            // 
+            this.DropLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DropLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DropLabel.ForeColor = System.Drawing.Color.White;
+            this.DropLabel.Location = new System.Drawing.Point(0, 0);
+            this.DropLabel.Margin = new System.Windows.Forms.Padding(3, 0, 3, 3);
+            this.DropLabel.Name = "DropLabel";
+            this.DropLabel.Size = new System.Drawing.Size(284, 262);
+            this.DropLabel.TabIndex = 1;
+            this.DropLabel.Text = "Drop Input PNG Here!";
+            this.DropLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox_MaxNodeHeight
+            // 
+            this.textBox_MaxNodeHeight.Location = new System.Drawing.Point(156, 0);
+            this.textBox_MaxNodeHeight.Name = "textBox_MaxNodeHeight";
+            this.textBox_MaxNodeHeight.Size = new System.Drawing.Size(128, 20);
+            this.textBox_MaxNodeHeight.TabIndex = 2;
+            this.textBox_MaxNodeHeight.Text = "255";
+            // 
+            // textBox_Width
+            // 
+            this.textBox_Width.Location = new System.Drawing.Point(156, 26);
+            this.textBox_Width.Name = "textBox_Width";
+            this.textBox_Width.Size = new System.Drawing.Size(128, 20);
+            this.textBox_Width.TabIndex = 3;
+            this.textBox_Width.Text = "?";
+            // 
+            // textBox_Height
+            // 
+            this.textBox_Height.Location = new System.Drawing.Point(156, 52);
+            this.textBox_Height.Name = "textBox_Height";
+            this.textBox_Height.Size = new System.Drawing.Size(128, 20);
+            this.textBox_Height.TabIndex = 4;
+            this.textBox_Height.Text = "?";
+            // 
+            // label_MaxNodeHeight
+            // 
+            this.label_MaxNodeHeight.AutoSize = true;
+            this.label_MaxNodeHeight.BackColor = System.Drawing.Color.Black;
+            this.label_MaxNodeHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_MaxNodeHeight.ForeColor = System.Drawing.Color.White;
+            this.label_MaxNodeHeight.Location = new System.Drawing.Point(24, 3);
+            this.label_MaxNodeHeight.Name = "label_MaxNodeHeight";
+            this.label_MaxNodeHeight.Size = new System.Drawing.Size(126, 13);
+            this.label_MaxNodeHeight.TabIndex = 5;
+            this.label_MaxNodeHeight.Text = "Max Node Height(m):";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(62, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Grid Width(m):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(56, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Grid Length(m):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Black;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(250, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "PROTIP: You can leave the Width/Length blank to";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(61, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "auto-determine grid size.";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_MaxNodeHeight);
+            this.Controls.Add(this.textBox_Height);
+            this.Controls.Add(this.textBox_Width);
+            this.Controls.Add(this.textBox_MaxNodeHeight);
+            this.Controls.Add(this.DropLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Png2Ter - © Flake 2017";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DropLabel;
+        private System.Windows.Forms.TextBox textBox_MaxNodeHeight;
+        private System.Windows.Forms.TextBox textBox_Width;
+        private System.Windows.Forms.TextBox textBox_Height;
+        private System.Windows.Forms.Label label_MaxNodeHeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Png2Srf/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Png2Srf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e) {
+          if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                if (!file.ToUpperInvariant().EndsWith(".PNG"))
+                {
+                    DropLabel.Text = "Dropped File is NOT a PNG File!\nCannot convert to TER!";
+                    DropLabel.ForeColor = Color.FromArgb(0, 255, 0, 0);
+                    continue;
+                }
+
+                int TotalNodes = 0;
+                double Width = 0;
+                double Height = 0;
+
+                try
+                {
+                    DropLabel.Text = "Working...";
+                    DropLabel.ForeColor = Color.FromArgb(0, 255, 220, 0);
+                    this.Refresh();
+                    Bitmap bitmap = new Bitmap(file);
+
+                    if (bitmap.Width * bitmap.Height > (128 * 128))
+                    {
+                        DialogResult result = MessageBox.Show("That's a very large PNG file to convert! (> 128^2 pixels to be converted to TER)... Are you sure about this?", "Uh Oh!", MessageBoxButtons.OKCancel);
+                        if (result == DialogResult.Cancel)
+                        {
+                            DropLabel.Text = "Drop Input PNG Here!";
+                            DropLabel.ForeColor = Color.FromArgb(0, 255, 255, 255);
+                            return;
+                        }
+                    }
+
+                    List<string> TERFileOutput = new List<string>();
+                    TERFileOutput.Add("TERRMESH");
+
+                    TotalNodes = bitmap.Width*bitmap.Height;
+
+                    Width = bitmap.Width;
+                    Height = bitmap.Height;
+                    Double.TryParse(textBox_Width.Text, out Width);
+                    Double.TryParse(textBox_Height.Text, out Height);
+                    if (Width <= 0) Width = bitmap.Width;
+                    if (Height <= 0) Height = bitmap.Height;
+
+                    double MaxNodeHeight = 255;
+                    Double.TryParse(textBox_MaxNodeHeight.Text, out MaxNodeHeight);
+                    double HeightScaling = MaxNodeHeight / 255;
+
+                    TERFileOutput.Add("NBL " + (bitmap.Width-1) + " " + (bitmap.Height-1));//1px == 1 node.
+                    TERFileOutput.Add("TMS " + Math.Round(((Width)/(bitmap.Width-1)), 2) + " " + Math.Round(((Height)/(bitmap.Height-1)), 2)); //as set by dialoge control
+
+                    //Add all the vertecies!
+                    for (int currentHeight = bitmap.Height-1; currentHeight >= 0; currentHeight--)
+                    {
+                        for (int currentWidth = 0; currentWidth < bitmap.Width; currentWidth++)
+                        {
+                            Color currentPixel = bitmap.GetPixel(currentWidth, currentHeight);
+                            float currentBrightness = currentPixel.GetBrightness();
+                            string faceVisible = (currentBrightness <= 0) ? "0" : "1";
+                            faceVisible = "1"; //Always visible, can set visibility of zero elev faces in scenery editor.
+
+                            //Color pixel = bitmap.GetPixel(j, i);
+                            TERFileOutput.Add("BLO " + //ADD BLOCK
+                                Math.Round((currentBrightness * HeightScaling) * 255, 0) + " " + //HEIGHT 0-100
+                                "R " + 
+                                faceVisible + " " +
+                                currentPixel.R + " " +
+                                currentPixel.G + " " +
+                                currentPixel.B + " " +
+                                faceVisible + " " +
+                                currentPixel.R + " " +
+                                currentPixel.G + " " +
+                                currentPixel.B
+                            ); //NODE
+                        }
+                    }
+                    TERFileOutput.Add("END");
+
+                    string outputFilePath = Path.GetDirectoryName(file) + "\\" + Path.GetFileNameWithoutExtension(file) +
+                                            ".TER";
+
+                    File.WriteAllLines(outputFilePath, TERFileOutput.ToArray());
+
+                    bitmap.Dispose();
+                }
+                catch
+                {
+                    DropLabel.Text = "Conversion Failed! Something Broke!";
+                    DropLabel.ForeColor = Color.FromArgb(0, 255, 0, 0);
+                    return;
+                }
+
+                DropLabel.Text = "Done (" + TotalNodes + " Nodes)!\nDrop Another PNG to Convert...";
+                DropLabel.ForeColor = Color.FromArgb(0, 0, 255, 0);
+                return;
+            }
+        }
+    }
+}
